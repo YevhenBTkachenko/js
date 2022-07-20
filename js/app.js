77917c81b9f26f958075c53363df85e7d138aadc
Yevhen <example@gmail.com>
2022-07-20 15:16:07 +0300

    second commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+// старый формат написания кода
+
+let number = 5;
+// let - изменяемая переменная
+const leftBorderWidth = 1;
+// const - неизменяемая переменная
+
+number = 15;
+
+console.log(leftBorderWidth);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj.a);
+
+console.log(name);
+var name = 'Yevhen';
+
+{
+    var result = 50;
+}
+console.log(result);
+
+
+// СПОСОБЫ НАПИСАНИЯ
+// snake_case
+// camelCase
+// Kebab-case
+// PascalCase
+// эти переменные руками не трогать:
+// UPPER_SNAKE_CASE
+// _apiKey
+
+let number = 4;
