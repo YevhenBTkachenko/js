8e1635ae1bf96b7ef67f952d0b9859e5ac9c204e
Yevhen Tkachenko <106694140+YevhenBTkachenko@users.noreply.github.com>
2022-07-23 21:43:48 +0300

    second task done

--- js/homework.js
+++ js/homework.js
@@ -1,1 +1,8 @@
 const numberOfFilms = +prompt('Сколько фильмов Вы уже просмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
